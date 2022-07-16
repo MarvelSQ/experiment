89482c2d555fb3a2f502e32a2cdc20ed8213e204
MarvelSQ <sun1993hlj@hotmail.com>
2022-07-16 15:00:54 +0800

    feat: version change

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ console.log("this is a experiment module");
 // this comment is new
 export default {
   name: "Experiment",
-  version: '0.0.2',
+  version: '0.0.3',
   something: "this is a update"
 };
