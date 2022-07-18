e2a4e7fd7b1892d370415871e3b7697db50b577d
MarvelSQ <sun1993hlj@hotmail.com>
2022-07-18 22:20:03 +0800

    feat(action): add PR preview
    auto deploy Preview Docs to subpath pr/<pr name>

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ console.log("this is a experiment module");
 // this comment is new
 export default {
   name: "Experiment",
-  version: '0.0.1',
+  version: '0.0.4',
   something: "this is a update"
 };
