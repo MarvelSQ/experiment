0ae80d69d62a42510ed43412f47aca88d1ea287a
MarvelSQ <sun1993hlj@hotmail.com>
2022-08-06 10:30:22 +0800

    feat(test): add coverage report

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from 'vitest'
+import main from '../index';
 
 function add(a: number, b: number) {
   return a + b;
@@ -7,5 +8,9 @@ function add(a: number, b: number) {
 describe('base test', () => {
   test('add function', () => {
     expect(add(1, 2)).toEqual(3);
+  })
+
+  test('main output', () => {
+    expect(main.name).toBe('Experiment');
   })
 })
